2e3c3478ad4a892e8e2f09bc52cb93ff424f9dbf
Andrée Ray <info@andreeray.se>
2017-02-15 10:15:35 +0100

    updated middleware for https redirect

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const   EXPRESS = require('express'),
         APP     = EXPRESS(),
         PORT = process.env.PORT || 4000
 
+APP.use(function(req,res,next){
+    if (req.headers['x-forwarded-proto'] === 'https') {
+        res.redirect('http://' + req.hostname + ':' + PORT +  req.url)
+    } else {
+        next()
+    }
+})
+
 APP.use(EXPRESS.static('public'))
 
 APP.listen(PORT)
